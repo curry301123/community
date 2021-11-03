18dd108b9b9651b5b2e5ce218c309334afbdf7c6
ccurry <1725675802@qq.com>
2021-11-03 11:33:09 +0800

    first submit

---
+++ src/main/java/com/nowcode/commuity/controller/LoginController.java
@@ -0,0 +1,94 @@
+package com.nowcode.commuity.controller;
+
+import com.google.code.kaptcha.Producer;
+import com.nowcode.commuity.domain.User;
+import com.nowcode.commuity.service.UserService;
+import com.nowcode.commuity.util.Constant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Map;
+
+@Controller
+public class LoginController implements Constant {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private Producer kaptchaProducer;
+
+    @RequestMapping(path = "/register",method = RequestMethod.GET)
+    public String getRegisterPage(){
+
+        return "/site/register";
+    }
+
+    @RequestMapping(path = "/register",method = RequestMethod.POST)
+    public String register(Model model, User user){
+        Map<String,Object> map = userService.register(user);
+        if(map == null || map.isEmpty()){
+            model.addAttribute("msg","注册成功，已发送激活邮件，请尽快激活");
+            model.addAttribute("target","/index");
+            return "/site/operate-result";
+        }else {
+            model.addAttribute("usernameMsg",map.get("usernameMsg"));
+            model.addAttribute("passwordMsg",map.get("passwordMsg"));
+            model.addAttribute("emailMsg",map.get("emailMsg"));
+            return "/site/register";
+        }
+    }
+
+    @RequestMapping(path = "/activation/{id}/{code}",method = RequestMethod.GET)
+    public String activation(Model model, @PathVariable("id") int id,@PathVariable("code") String code){
+        int activation = userService.activation(id, code);
+        if(activation == ACTIVATION_SUCCESS){
+            model.addAttribute("msg","激活成功，您账号已经可以正常使用");
+            model.addAttribute("target","/login");
+        }else if (activation == ACTIVATION_FAILED){
+            model.addAttribute("msg","无效操作，该账号已经激活过！");
+            model.addAttribute("target","/index");
+        }else {
+            model.addAttribute("msg","激活失败，激活码错误");
+            model.addAttribute("target","/index");
+        }
+        return "/site/operate-result";
+    }
+
+    @RequestMapping(path = "/login",method = RequestMethod.GET)
+    public String getLogin(){
+        return "/site/login";
+    }
+    @RequestMapping(path = "/kaptcha",method = RequestMethod.GET)
+    public void getKaptcha(HttpServletResponse response, HttpSession session){
+        //生成验证码
+        String text = kaptchaProducer.createText();
+        BufferedImage image = kaptchaProducer.createImage(text);
+        //将验证码存入session
+        session.setAttribute("kaptcha",text);
+        //将图片输出给浏览器
+        response.setContentType("image/png");
+
+        try {
+            ServletOutputStream outputStream = response.getOutputStream();
+            ImageIO.write(image,"png",outputStream);
+        } catch (IOException e) {
+            logger.error("响应验证码失败" + e.getMessage());
+        }
+    }
+}
